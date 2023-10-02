5580fd0caf53e792a492a2c52e588b218ec5a1f9
Alexandr Rybakov <ralan1996@yandex.ru>
2023-10-02 14:20:10 +0400

    fix_bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import './index.scss';
 
 import App from './components/App';
+import { Provider } from 'react-redux';
+import store from './store';
+import AppContent from './components/AppContent';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+
+const Router =
+  process.env.REACT_APP_GH_PAGES === 'true' ? HashRouter : BrowserRouter;
+
+root.render(
+  <Provider store={store}>
+    <Router>
+      <AppContent />
+    </Router>
+  </Provider>
+);
